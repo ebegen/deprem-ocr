c08a2f2c8ea1b33c78b47561abcaef5b28ce5e2a
Mert Cobanov <mertcobanov@gmail.com>
2023-02-06 19:32:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -14,15 +14,19 @@ def get_text(input_img):
     return " ".join(result)
 
 
-def save_json(mahalle, il, sokak, apartman):
+def save_csv(mahalle, il, sokak, apartman):
 
     adres_full = [mahalle, il, sokak, apartman]
 
     with open("adress_book.csv", "a", encoding="utf-8") as f:
         write = csv.writer(f)
-
         write.writerow(adres_full)
+    return adres_full
 
+def get_json(mahalle, il, sokak, apartman):
+    adres = {'mahalle':mahalle, 'il':il, 'sokak':sokak, 'apartman':apartman}
+    dump = json.dumps(adres, indent=4, ensure_ascii=False)
+    return dump
 
 with gr.Blocks() as demo:
     gr.Markdown(""" # Image to Text - Adres""")
@@ -34,16 +38,27 @@ with gr.Blocks() as demo:
     submit_button = gr.Button()
     submit_button.click(get_text, img_area, ocr_result)
 
-    with gr.Row():
-        with gr.Column():
+    
+    with gr.Column():
+        with gr.Row():
             mahalle = gr.Textbox(label="mahalle")
             sokak = gr.Textbox(label="sokak")
+        with gr.Row():
             apartman = gr.Textbox(label="apartman")
             il = gr.Textbox(label="il")
-            tarif = gr.Textbox(label="Tarif")
+        tarif = gr.Textbox(label="Tarif")
+    
+    json_out = gr.Textbox()
+    csv_out = gr.Textbox()
 
     adres_submit = gr.Button()
-    adres_submit.click(save_json, [mahalle, il, sokak, apartman], None)
+    adres_submit.click(save_csv, [mahalle, il, sokak, apartman], None)
+    adres_submit.click(get_json, [mahalle, il, sokak, apartman], json_out)
+    adres_submit.click(save_csv, [mahalle, il, sokak, apartman], csv_out)
+
+
+    
+
 
 if __name__ == "__main__":
-    demo.launch(share=True)
+    demo.launch()
